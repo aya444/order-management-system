d04bced772cb050440a63c49bff449d358c66634
Aya Shabana <59667304+aya444@users.noreply.github.com>
2024-11-10 15:43:44 +0200

    Added Api gateway and service registry

---
+++ inventory-service/src/test/java/com/aya/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aya.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
